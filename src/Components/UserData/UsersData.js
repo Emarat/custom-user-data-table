b85c3c57494bc9fc3ca1142a2965cea9e843daa4
Emarat <emarathossain00@gmail.com>
2022-10-07 17:56:12 +0600

    custom table

--- src/Components/UserData/UsersData.js
+++ src/Components/UserData/UsersData.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 const usersData = () => {
 
@@ -10,8 +11,31 @@ const usersData = () => {
     }, [])
     return (
         <div>
-
-
+            <h2 className='my-5'>Users Custom Table:</h2>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Company Name</th>
+                        <th>Zipcode</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        userData.map(data => <>
+                            <tr key={data.id} user={data}>
+                                <td>{data.id}</td>
+                                <td>{data.name}</td>
+                                <td>{data.email}</td>
+                                <td>{data.company.name}</td>
+                                <td>{data.address.zipcode}</td>
+                            </tr>
+                        </>)
+                    }
+                </tbody>
+            </Table>
         </div>
     );
 };
